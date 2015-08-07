47bf1fedb567358fe14e9994ee182844deee0bb7
thomassnielsen <me@thomassnielsen.com>
2015-08-07 18:29:49 +0200

    Add abstraction for compare_digest

--- app.py
+++ app.py
@@ -7,6 +7,7 @@
 import os
 import re
 import io
+import sys
 import json
 import subprocess
 import ipaddress
@@ -81,7 +82,7 @@ def hello():
           if type(key) == unicode:
             key = key.encode()
           mac = hmac.new(key, msg=request.data, digestmod=sha1)
-          if not hmac.compare_digest(mac.hexdigest(), signature):
+          if not compare_digest(mac.hexdigest(), signature):
             abort(403)
 
         if repo.get('action', None):
@@ -96,6 +97,26 @@ def hello():
       'trace':"%s" % traceback.format_exc()
     })
 
+# Check if python version is less than 2.7.7
+if sys.version_info < (2, 7, 7):
+    # http://blog.turret.io/hmac-in-go-python-ruby-php-and-nodejs/
+    def compare_digest(a, b):
+        """
+        ** From Django source **
+        Run a constant time comparison against two strings
+        Returns true if a and b are equal.
+        a and b must both be the same length, or False is
+        returned immediately
+        """
+        if len(a) != len(b):
+            return False
+
+        result = 0
+        for ch_a, ch_b in zip(a, b):
+            result |= ord(ch_a) ^ ord(ch_b)
+        return result == 0
+else:
+    compare_digest = hmac.compare_digest
 
 if __name__ == "__main__":
   app.run()
